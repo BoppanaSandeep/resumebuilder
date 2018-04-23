7110eab6c2eeea1d34b516f655ed27426e6cc08d
BoppanaSandeep <sboppana@ameripro-solutions.com>
2018-04-23 18:38:52 +0530

    Design Changes.

--- src/pages/shared/urls.ts
+++ src/pages/shared/urls.ts
@@ -1,18 +1,18 @@
 export class Urls {
-    public connection = "http://192.168.43.54/resumebuilder/registrationpage/conn";
-    public registration_url = "http://192.168.43.54/resumebuilder/registrationpage";
-    public login_url = "http://192.168.43.54/resumebuilder/rb/login";
-    public login_status_url = "http://192.168.43.54/resumebuilder/rb/login_status/";
-    public forgotpwd_url = "http://192.168.43.54/resumebuilder/rb/forgot_pwd";
-    public skills_form_url = "http://192.168.43.54/resumebuilder/rb/skills";
-    public skills_data_url = "http://192.168.43.54/resumebuilder/rb/skills_data/";
-    public expedu_form_url = "http://192.168.43.54/resumebuilder/rb/expedu";
-    public expedu_data_url = "http://192.168.43.54/resumebuilder/rb/expedu_data/";
-    public skills_delete_data_url = "http://192.168.43.54/resumebuilder/rb/skill_delete_data";
-    public skills_options_fetching_url = "http://192.168.43.54/resumebuilder/rb/itskilloptions/";
-    public exp_delete_data_url = "http://192.168.43.54/resumebuilder/rb/exp_delete_data"
-    public edu_delete_data_url = "http://192.168.43.54/resumebuilder/rb/edu_delete_data"
-    public edit_expedu_data_url = "http://192.168.43.54/resumebuilder/rb/edit_expedu_data";
-    public profile_image_upload = "http://192.168.43.54/resumebuilder/rb/profile_image_upload";
-    public fetching_job_posts = "http://192.168.43.54/resumebuilder/rb/fetching_job_posts/";
+    public connection = "http://10.0.0.167/resumebuilder/registrationpage/conn";
+    public registration_url = "http://10.0.0.167/resumebuilder/registrationpage";
+    public login_url = "http://10.0.0.167/resumebuilder/rb/login";
+    public login_status_url = "http://10.0.0.167/resumebuilder/rb/login_status/";
+    public forgotpwd_url = "http://10.0.0.167/resumebuilder/rb/forgot_pwd";
+    public skills_form_url = "http://10.0.0.167/resumebuilder/rb/skills";
+    public skills_data_url = "http://10.0.0.167/resumebuilder/rb/skills_data/";
+    public expedu_form_url = "http://10.0.0.167/resumebuilder/rb/expedu";
+    public expedu_data_url = "http://10.0.0.167/resumebuilder/rb/expedu_data/";
+    public skills_delete_data_url = "http://10.0.0.167/resumebuilder/rb/skill_delete_data";
+    public skills_options_fetching_url = "http://10.0.0.167/resumebuilder/rb/itskilloptions/";
+    public exp_delete_data_url = "http://10.0.0.167/resumebuilder/rb/exp_delete_data"
+    public edu_delete_data_url = "http://10.0.0.167/resumebuilder/rb/edu_delete_data"
+    public edit_expedu_data_url = "http://10.0.0.167/resumebuilder/rb/edit_expedu_data";
+    public profile_image_upload = "http://10.0.0.167/resumebuilder/rb/profile_image_upload";
+    public fetching_job_posts = "http://10.0.0.167/resumebuilder/rb/fetching_job_posts/";
 }
